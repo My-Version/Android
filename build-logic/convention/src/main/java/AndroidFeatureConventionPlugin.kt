740040221470befa0692950fd9161fd96198ce11
boiledegg <tjrwns2163@naver.com>
2024-08-08 18:31:12 +0900

    [ADD/#3] Navigation 의존성 추가

--- build-logic/convention/src/main/java/AndroidFeatureConventionPlugin.kt
+++ build-logic/convention/src/main/java/AndroidFeatureConventionPlugin.kt
@@ -7,12 +7,16 @@ import org.gradle.kotlin.dsl.dependencies
 internal class AndroidFeatureConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
+            pluginManager.apply("androidx.navigation.safeargs.kotlin")
+
             dependencies {
                 add("implementation", project(":core:designsystem"))
                 add("implementation", project(":core:common"))
                 add("implementation", project(":core:domain"))
 
                 add("implementation", libs.findLibrary("timber").get())
+                add("implementation", libs.findLibrary("androidx.navigation.compose").get())
+                add("implementation", libs.findLibrary("kotlinx.serialization.json").get())
             }
         }
     }
--- build.gradle.kts
+++ build.gradle.kts
@@ -7,4 +7,13 @@ plugins {
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.dagger.hilt) apply false
     alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.safe.args) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.kotlin.parcelize) apply false
+}
+
+buildscript {
+    dependencies {
+        classpath(libs.androidx.navigation.safe.args)
+    }
 }
