5052c876ff7d2bc229993ca29d8f8d3f459a542f
boiledegg <tjrwns2163@naver.com>
2024-08-08 19:10:13 +0900

    [FEAT/#3] Main 탐색 추가

---
+++ feature/main/src/main/java/com/my/version/feature/main/MainNavigator.kt
@@ -0,0 +1,32 @@
+package com.my.version.feature.main
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.navigation.NavDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.my.version.feature.home.navigation.Home
+
+class MainNavigator(
+    val navController: NavHostController
+) {
+    private val currentDestination: NavDestination?
+        @Composable get() = navController
+            .currentBackStackEntryAsState().value?.destination
+
+    val startDestination = Home
+
+
+
+    fun navigateUp() {
+        navController.navigateUp()
+    }
+}
+
+@Composable
+fun rememberMainNavigator(
+    navController: NavHostController = rememberNavController()
+): MainNavigator = remember(navController) {
+    MainNavigator(navController)
+}
--- feature/main/src/main/java/com/my/version/feature/main/MainScreen.kt
+++ feature/main/src/main/java/com/my/version/feature/main/MainScreen.kt
@@ -3,17 +3,42 @@ package com.my.version.feature.main
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import com.my.version.feature.auth.signin.navigation.signInScreen
+import com.my.version.feature.auth.signup.navigation.signUpScreen
+import com.my.version.feature.cover.navigation.coverScreen
+import com.my.version.feature.evaluate.navigation.evaluateScreen
+import com.my.version.feature.home.navigation.homeScreen
 
 @Composable
-fun MainScreen() {
+fun MainScreen(
+    mainNavigator: MainNavigator = rememberMainNavigator(),
+) {
     Scaffold { paddingValues ->
         Column(
             modifier = Modifier.padding(paddingValues)
         ) {
-            Text(text = "Hello World")
+            MainNavHost(
+                mainNavigator = mainNavigator
+            )
         }
+    }
+}
+
+@Composable
+fun MainNavHost(
+    mainNavigator: MainNavigator
+) {
+    NavHost(
+        navController = mainNavigator.navController,
+        startDestination = mainNavigator.startDestination
+    ) {
+        homeScreen()
+        coverScreen()
+        evaluateScreen()
+        signInScreen()
+        signUpScreen()
     }
 }
