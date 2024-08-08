671633e7a10ca1b15b978b3ba0d375f29c921e8f
boiledegg <tjrwns2163@naver.com>
2024-08-08 18:39:21 +0900

    [FEAT/#3] Evaluate 모듈 탐색 추가

---
+++ feature/evaluate/src/main/java/com/my/version/feature/evaluate/EvaluateScreen.kt
@@ -0,0 +1,8 @@
+package com.my.version.feature.evaluate
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun EvaluateRoute() {
+
+}
---
+++ feature/evaluate/src/main/java/com/my/version/feature/evaluate/navigation/EvaluateNavigation.kt
@@ -0,0 +1,20 @@
+package com.my.version.feature.evaluate.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+import com.my.version.core.common.navigation.Route
+import com.my.version.feature.evaluate.EvaluateRoute
+import kotlinx.serialization.Serializable
+
+fun NavController.navigateToEvaluate(navOptions: NavOptions) = navigate(Evaluate, navOptions)
+
+fun NavGraphBuilder.evaluateScreen() {
+    composable<Evaluate>{
+        EvaluateRoute()
+    }
+}
+
+@Serializable
+data object Evaluate: Route
