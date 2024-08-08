73924d795a15dd68ac8d458b6c15ce591598b8c7
boiledegg <tjrwns2163@naver.com>
2024-08-08 18:39:26 +0900

    [FEAT/#3] Home 모듈 탐색 추가

---
+++ feature/home/src/main/java/com/my/version/feature/home/HomeScreen.kt
@@ -0,0 +1,8 @@
+package com.my.version.feature.home
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeRoute() {
+
+}
---
+++ feature/home/src/main/java/com/my/version/feature/home/navigation/HomeNavigation.kt
@@ -0,0 +1,20 @@
+package com.my.version.feature.home.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+import com.my.version.core.common.navigation.Route
+import com.my.version.feature.home.HomeRoute
+import kotlinx.serialization.Serializable
+
+fun NavController.navigateToHome(navOptions: NavOptions) = navigate(Home, navOptions)
+
+fun NavGraphBuilder.homeScreen() {
+    composable<Home>{
+        HomeRoute()
+    }
+}
+
+@Serializable
+data object Home: Route
