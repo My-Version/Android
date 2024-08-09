311c30682fe0b5caa7f738b6895245551baad4e2
boiledegg <tjrwns2163@naver.com>
2024-08-09 21:59:04 +0900

    [ADD/#6] 색상 추가

--- core/designsystem/src/main/java/com/my/version/core/designsystem/theme/Color.kt
+++ core/designsystem/src/main/java/com/my/version/core/designsystem/theme/Color.kt
@@ -2,10 +2,24 @@ package com.my.version.core.designsystem.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Main Color
+val MyVersionMain = Color(0xFFAB3DFF)
+val MyVersionSub1 = Color(0xFFBF6FFC)
+val MyVersionSub2 = Color(0xFFB553FF)
+val MyVersionSub3 = Color(0xFFAB3DFF)
+val MyVersionSub4 = Color(0xFF8D00F8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// BackGround
+val MyVersionBackground = Color(0xFF575757)
+
+// Grey Scale
+val White = Color(0xFFFFFFFF)
+val Grey100 = Color(0xFFF5F5F5)
+val Grey150 = Color(0xFFE9E9E9)
+val Grey200 = Color(0xFFDDDDDD)
+val Grey300 = Color(0xFFBCBCBC)
+val Grey350 = Color(0xFFADADAD)
+val Grey400 = Color(0xFF666666)
+val Grey500 = Color(0xFF373737)
+val Black = Color(0xFF171717)
+
