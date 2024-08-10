6e2c296c99229ecd5d04883f23984c7539068820
boiledegg <tjrwns2163@naver.com>
2024-08-11 03:08:19 +0900

    [UI/#10] 정렬 버튼 구현

--- core/designsystem/src/main/java/com/my/version/core/designsystem/component/button/SortingButton.kt
+++ core/designsystem/src/main/java/com/my/version/core/designsystem/component/button/SortingButton.kt
@@ -2,28 +2,25 @@ package com.my.version.core.designsystem.component.button
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.ripple.LocalRippleTheme
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.my.version.core.common.extension.noRippleClickable
 import com.my.version.core.designsystem.theme.MyVersionBackground
 import com.my.version.core.designsystem.theme.MyVersionTheme
 import com.my.version.core.designsystem.theme.White
 import com.my.version.core.designsystem.type.SortingButtonType
-import com.terning.core.util.NoRippleTheme
 
 @Composable
 fun SortingButton(
@@ -38,27 +35,26 @@ fun SortingButton(
         SortingButtonType.UNFOLD
     }
 
-    CompositionLocalProvider(LocalRippleTheme provides NoRippleTheme) {
-        Button(
-            onClick = onClick,
-            shape = RoundedCornerShape(5.dp),
-            colors = ButtonDefaults.buttonColors(
-                contentColor = White,
-                containerColor = Color.Transparent
-            ),
-            modifier = modifier
-        ) {
-            Text(
-                text = text,
-                style = MaterialTheme.typography.labelMedium
-            )
-            Spacer(modifier = Modifier.width(6.dp))
-            Icon(
-                painter = painterResource(id = sortType.icon),
-                contentDescription = stringResource(id = sortType.contentDescription)
-            )
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier.noRippleClickable {
+            onClick()
         }
+    ) {
+        Text(
+            color = White,
+            style = MaterialTheme.typography.labelMedium,
+            text = text,
+            textAlign = TextAlign.End
+        )
+        Spacer(modifier = Modifier.width(6.dp))
+        Icon(
+            painter = painterResource(id = sortType.icon),
+            contentDescription = stringResource(id = sortType.contentDescription),
+            tint = White
+        )
     }
+
 }
 
 @Preview(showBackground = true)
