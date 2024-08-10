b3ef418e145c1d118b6ed49efea948442be096b0
boiledegg <tjrwns2163@naver.com>
2024-08-10 21:23:05 +0900

    [UI/#9] 탑바 색상 수정

--- core/designsystem/src/main/java/com/my/version/core/designsystem/component/topappbar/MyVersionBasicTopAppBar.kt
+++ core/designsystem/src/main/java/com/my/version/core/designsystem/component/topappbar/MyVersionBasicTopAppBar.kt
@@ -5,9 +5,11 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.my.version.core.designsystem.theme.White
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -24,7 +26,12 @@ internal fun MyVersionBasicTopAppBar(
             )
         },
         navigationIcon = navigationIcon,
-        modifier = modifier.padding(horizontal = 12.dp)
+        colors = TopAppBarDefaults.topAppBarColors(
+            navigationIconContentColor = White,
+            titleContentColor = White,
+            containerColor = MaterialTheme.colorScheme.background
+        ),
+        modifier = Modifier.padding(horizontal = 12.dp)
     )
 }
 
