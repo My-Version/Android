593f0443cb2bef92ff3f127c113eb6ecdccb8d93
boiledegg <tjrwns2163@naver.com>
2024-08-10 15:42:38 +0900

    [FEAT/#4] Modifier 확장함수

--- core/common/src/main/java/com/my/version/core/common/extension/ModifierExt.kt
+++ core/common/src/main/java/com/my/version/core/common/extension/ModifierExt.kt
@@ -1,11 +1,24 @@
 package com.my.version.core.common.extension
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
 
 fun Modifier.noScaffoldPadding() = this
     .statusBarsPadding()
     .navigationBarsPadding()
     .systemBarsPadding()
+
+@SuppressLint("ModifierFactoryUnreferencedReceiver")
+inline fun Modifier.noRippleClickable(crossinline onClick: () -> Unit): Modifier = composed {
+    clickable(indication = null,
+        interactionSource = remember { MutableInteractionSource() }) {
+        onClick()
+    }
+}
