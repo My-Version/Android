579136bd319652c5181c0e00d6ff62f5ebf96aa0
boiledegg <tjrwns2163@naver.com>
2024-08-12 23:20:58 +0900

    [UI/#12] 상태바 요소 색상 변경

--- core/designsystem/src/main/java/com/my/version/core/designsystem/theme/Theme.kt
+++ core/designsystem/src/main/java/com/my/version/core/designsystem/theme/Theme.kt
@@ -1,10 +1,14 @@
 package com.my.version.core.designsystem.theme
 
+import android.app.Activity
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
-private val LightColorScheme = lightColorScheme(
+private val DarkColorScheme = darkColorScheme(
     primary = MyVersionMain,
     background = MyVersionBackground
 )
@@ -13,12 +17,20 @@ private val LightColorScheme = lightColorScheme(
 fun MyVersionTheme(
     content: @Composable () -> Unit
 ) {
-    val colorScheme = LightColorScheme
+    val colorScheme = DarkColorScheme
+
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect{
+            val window = (view.context as Activity).window
+            val controller = WindowCompat.getInsetsController(window, view)
+            controller.isAppearanceLightStatusBars = false
+        }
+    }
 
     MaterialTheme(
         colorScheme = colorScheme,
         content = content,
         typography = MyVersionTypography
     )
-
 }
