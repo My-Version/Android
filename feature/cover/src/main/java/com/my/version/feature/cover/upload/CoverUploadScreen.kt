d3bcd711b420a25ae4e9551141200123875fde72
boiledegg <tjrwns2163@naver.com>
2024-09-21 18:00:59 +0900

    [DEL/#29] 라이브러리 정리

--- feature/cover/src/main/java/com/my/version/feature/cover/upload/CoverUploadScreen.kt
+++ feature/cover/src/main/java/com/my/version/feature/cover/upload/CoverUploadScreen.kt
@@ -1,7 +1,6 @@
 package com.my.version.feature.cover.upload
 
 import android.content.Intent
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -22,7 +21,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.LocalLifecycleOwner
@@ -34,9 +32,7 @@ import com.my.version.core.designsystem.component.text.SingleLineText
 import com.my.version.core.designsystem.component.topappbar.NavigateUpTopAppBar
 import com.my.version.core.designsystem.theme.Black
 import com.my.version.core.designsystem.theme.Grey200
-import com.my.version.core.designsystem.theme.Grey300
 import com.my.version.core.designsystem.theme.Grey350
-import com.my.version.core.designsystem.theme.Grey400
 import com.my.version.core.designsystem.theme.MyVersionSub5
 import com.my.version.core.designsystem.theme.MyVersionTypography
 import com.my.version.core.designsystem.type.VerticalItemType
