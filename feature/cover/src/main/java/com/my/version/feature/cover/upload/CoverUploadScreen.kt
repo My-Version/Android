f559de307b497d760b6ad16ef5542c42b1552c01
boiledegg <tjrwns2163@naver.com>
2024-09-21 18:00:19 +0900

    [UI/#29] 빈화면 가이드 추가

--- feature/cover/src/main/java/com/my/version/feature/cover/upload/CoverUploadScreen.kt
+++ feature/cover/src/main/java/com/my/version/feature/cover/upload/CoverUploadScreen.kt
@@ -1,6 +1,7 @@
 package com.my.version.feature.cover.upload
 
 import android.content.Intent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -14,12 +15,14 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.LocalLifecycleOwner
@@ -31,7 +34,11 @@ import com.my.version.core.designsystem.component.text.SingleLineText
 import com.my.version.core.designsystem.component.topappbar.NavigateUpTopAppBar
 import com.my.version.core.designsystem.theme.Black
 import com.my.version.core.designsystem.theme.Grey200
+import com.my.version.core.designsystem.theme.Grey300
+import com.my.version.core.designsystem.theme.Grey350
+import com.my.version.core.designsystem.theme.Grey400
 import com.my.version.core.designsystem.theme.MyVersionSub5
+import com.my.version.core.designsystem.theme.MyVersionTypography
 import com.my.version.core.designsystem.type.VerticalItemType
 import com.my.version.core.domain.entity.RecordAudioFile
 import com.my.version.feature.cover.R
@@ -112,8 +119,6 @@ fun CoverUploadScreen(
             title = stringResource(id = R.string.cover_topbar_upload)
         )
 
-        //BasicSpacer(height = 10.dp)
-
         Row(
             modifier = Modifier
                 .fillMaxWidth()
@@ -156,24 +161,19 @@ fun CoverUploadScreen(
 
         BasicSpacer(height = 12.dp)
 
-        LazyColumn(
+        Box(
             modifier = Modifier
                 .padding(horizontal = 20.dp)
                 .weight(1f),
-            contentPadding = PaddingValues(bottom = 20.dp)
         ) {
-            itemsIndexed(fileList) { index, file ->
-                MyVersionVerticalItemTwoButton(
-                    firstItemType = VerticalItemType.MUSIC,
-                    secondItemType = VerticalItemType.AUDIO,
-                    onClickFirstItem = { onPlayButtonClicked(index) },
-                    onClickSecondItem = { onCancelButtonClicked(index) },
-                    title = file.title,
-                    subTitle = file.time
+            if (fileList.isEmpty()) {
+                EmptyListScreen()
+            } else {
+                AudioFileListScreen(
+                    onCancelButtonClicked = onCancelButtonClicked,
+                    onPlayButtonClicked = onPlayButtonClicked,
+                    fileList = fileList
                 )
-                if (fileList.last() != file) {
-                    BasicSpacer(height = 16.dp)
-                }
             }
         }
 
@@ -190,5 +190,66 @@ fun CoverUploadScreen(
                 onClick = onUploadComplete
             )
         }
+    }
+}
+
+@Composable
+private fun AudioFileListScreen(
+    modifier: Modifier = Modifier,
+    fileList: List<RecordAudioFile>,
+    onCancelButtonClicked: (Int) -> Unit,
+    onPlayButtonClicked: (Int) -> Unit
+) {
+    LazyColumn(
+        modifier = modifier,
+        contentPadding = PaddingValues(bottom = 20.dp)
+    ) {
+        itemsIndexed(fileList) { index, file ->
+            MyVersionVerticalItemTwoButton(
+                firstItemType = VerticalItemType.MUSIC,
+                secondItemType = VerticalItemType.AUDIO,
+                onClickFirstItem = { onPlayButtonClicked(index) },
+                onClickSecondItem = { onCancelButtonClicked(index) },
+                title = file.title,
+                subTitle = file.time
+            )
+            if (fileList.last() != file) {
+                BasicSpacer(height = 16.dp)
+            }
+        }
+    }
+}
+
+@Composable
+private fun EmptyListScreen(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+
+        BasicSpacer(height = 100.dp)
+        Text(
+            text = stringResource(id = R.string.cover_empty_guide1),
+            style = MyVersionTypography.bodyLarge,
+            color = Grey350
+        )
+
+        BasicSpacer(height = 40.dp)
+
+        Text(
+            text = stringResource(id = R.string.cover_empty_guide2),
+            style = MyVersionTypography.bodyMedium,
+            color = Grey350
+        )
+        BasicSpacer(height = 20.dp)
+
+        Text(
+            text = stringResource(id = R.string.cover_empty_guide3),
+            style = MyVersionTypography.bodyMedium,
+            color = Grey350
+        )
+
     }
 }
