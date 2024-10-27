e519d20dbb41e3b8505b1a675f1c6ebfec996b92
boiledegg <tjrwns2163@naver.com>
2024-10-27 14:12:27 +0900

    [CHORE/#44] 모듈 수정

--- core/common/src/main/java/com/my/version/core/common/musicplayer/di/MediaPlayerModule.kt
+++ core/common/src/main/java/com/my/version/core/common/musicplayer/di/MediaPlayerModule.kt
@@ -2,8 +2,8 @@ package com.my.version.core.common.musicplayer.di
 
 import android.content.Context
 import com.my.version.core.common.musicplayer.StreamMediaPlayer
-import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
@@ -11,11 +11,11 @@ import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
-abstract class MediaPlayerModule {
+object MediaPlayerModule {
 
-    @Binds
+    @Provides
     @Singleton
-    fun bindMediaPlayer(
+    fun provideMediaPlayer(
         @ApplicationContext context: Context
     ): StreamMediaPlayer = StreamMediaPlayer(context)
 
