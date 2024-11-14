f9bffcc46f25d3d7029462d0d1540962f0506792
boiledegg <tjrwns2163@naver.com>
2024-11-14 16:28:44 +0900

    [FIX/#58] 다운로드 오류 수정

--- core/common/src/main/java/com/my/version/core/common/extension/DownloadManagerExt.kt
+++ core/common/src/main/java/com/my/version/core/common/extension/DownloadManagerExt.kt
@@ -4,16 +4,15 @@ import android.app.DownloadManager
 import android.net.Uri
 import android.os.Environment
 import androidx.compose.runtime.compositionLocalOf
+import timber.log.Timber
 import java.io.File
 
 fun DownloadManager.download(
-    downloadUri: Uri,
-    outputPath: String,
-    notificationTitle: String
-) {
+    downloadUri: Uri, outputPath: String, notificationTitle: String
+) = try {
     val downloadPath =
         Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).absolutePath + "/$outputPath"
-
+    Timber.tag("DownloadManager").d("download: $downloadUri, outputPath: $outputPath")
     val destinationUri = Uri.fromFile(File(downloadPath))
 
     val request = DownloadManager.Request(downloadUri).apply {
@@ -22,6 +21,8 @@ fun DownloadManager.download(
         setAllowedOverMetered(true)
     }
     enqueue(request)
+} catch (e: Exception) {
+    e.printStackTrace()
 }
 
 
